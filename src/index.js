562c470650b42285e335a629e51e64cce7d42911
aly2705 <alexandraaly.simion@gmail.com>
2022-09-04 14:22:53 +0300

    added polyfill for dnd on mobile

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import store from './store';
 import { Provider } from 'react-redux/es/exports';
+import './polyfills/DragDropTouch';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
