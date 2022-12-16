15955179a9e3ec7a1329fcf7788db0decc168d16
aly2705 <alexandraaly.simion@gmail.com>
2022-12-16 13:04:48 +0200

    fixed streaks bug when changing year

--- src/components/Statistics/Streaks.js
+++ src/components/Statistics/Streaks.js
@@ -6,6 +6,7 @@ import { numDaysInMonths } from '../../helpers/config';
 import { useState } from 'react';
 
 const formatSquares = (length, type) => {
+  if (length === -1) length = 6;
   const squares = [];
   while (length) {
     if (type === 'omitted')
@@ -21,14 +22,17 @@ const formatSquares = (length, type) => {
   }
   return squares;
 };
+console.log(formatSquares(-1, 'omitted'));
 
 const Streaks = () => {
   const calendar = useSelector(state => state.calendar.calendar);
   const currentMonth = new Date().getMonth();
   const currentYear = new Date().getFullYear();
-  const [streaksMonth, setStreaksMonth] = useState(currentMonth);
 
-  const monthString = new Date(currentYear, streaksMonth, 1).toLocaleString(
+  const [streaksMonth, setStreaksMonth] = useState(currentMonth);
+  const [streaksYear, setStreaksYear] = useState(currentYear);
+
+  const monthString = new Date(streaksYear, streaksMonth, 1).toLocaleString(
     'en-US',
     {
       month: 'long',
@@ -36,7 +40,8 @@ const Streaks = () => {
   );
 
   const firstWeekdayOfMonth =
-    new Date(currentYear, streaksMonth, 1).getDay() - 1;
+    new Date(streaksYear, streaksMonth, 1).getDay() - 1;
+  console.log(firstWeekdayOfMonth);
 
   const emptySquares = formatSquares(firstWeekdayOfMonth, 'omitted');
 
@@ -47,20 +52,44 @@ const Streaks = () => {
   const futureSquares = formatSquares(numOfFutureDays, 'future');
   const monthArray = [...streaksMonthData, ...futureSquares];
 
+  // HANDLERS FOR CHANGING STREAKS MONTH AND YEAR
   const showPastMonthHandler = () => {
     const newMonth = streaksMonth - 1;
-    if (newMonth >= 0 && currentMonth - newMonth < 3) setStreaksMonth(newMonth);
+    if (newMonth >= 0) setStreaksMonth(newMonth);
+    else if (newMonth < 0) {
+      setStreaksMonth(11);
+      setStreaksYear(prev => prev - 1);
+    }
   };
   const showNextMonthHandler = () => {
     const newMonth = streaksMonth + 1;
-    if (newMonth > currentMonth) return;
-    setStreaksMonth(newMonth);
+    console.log(currentMonth, streaksMonth);
+
+    if (newMonth > currentMonth && streaksYear === currentYear) return;
+    if (newMonth <= 11) setStreaksMonth(newMonth);
+    if (newMonth > 11) {
+      setStreaksMonth(0);
+      setStreaksYear(prev => prev + 1);
+    }
   };
+
+  // CONDITIONS FOR BUTTONS
+  // Show just 12 months behind
+  const showBackButton =
+    (streaksMonth <= currentMonth &&
+      streaksYear === currentYear &&
+      currentMonth !== 11) ||
+    (streaksMonth > currentMonth + 1 && streaksYear === currentYear - 1) ||
+    (currentMonth === 11 && streaksMonth > 0);
+
+  // Show till current month
+  const showNextButton =
+    streaksMonth < currentMonth || streaksYear < currentYear;
 
   return (
     <Card className={classes.streaks}>
       <h3>Streaks</h3>
-      <h4>{`${monthString} ${currentYear}`}</h4>
+      <h4>{`${monthString} ${streaksYear}`}</h4>
       <div className={classes.streaks__chart}>
         <div className={classes.streaks__grid}>
           {['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'].map(day => (
@@ -77,7 +106,7 @@ const Streaks = () => {
               }`}
             ></div>
           ))}
-          {currentMonth - streaksMonth !== 2 && (
+          {showBackButton && (
             <button
               className={`${classes.arrow} ${classes['arrow--left']}`}
               onClick={showPastMonthHandler}
@@ -87,7 +116,7 @@ const Streaks = () => {
               </svg>
             </button>
           )}
-          {streaksMonth < currentMonth && (
+          {showNextButton && (
             <button
               className={`${classes.arrow} ${classes['arrow--right']}`}
               onClick={showNextMonthHandler}
